5d1545400146d022daee14fbe2951b2b0ff9b1a5
Claire Lv <thulvcc2017@gmail.com>
2022-08-27 10:18:15 +0800

    Initialize commit

---
+++ SensitiveWordFilter.py
@@ -0,0 +1,12 @@
+from email import header
+
+
+#!/usr/bin/env python
+# -*- encoding: utf-8 -*-
+'''
+@Description       :Filters out the sensitive words in the text.
+@Date     :2022/08/27 10:02:23
+@Author      :Lv Chuancheng
+@version      :1.0
+'''
+
